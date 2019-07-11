2f75fce9ea9413687c0aba460ebdc045006ecb69
senjansen <senjansen.sj@gmail.com>
2019-07-11 17:22:20 +0700

    feedback - tampilkan nilai -1 jika ada lebih 1 angka yang menjadi modus

--- exercise-4.js
+++ exercise-4.js
@@ -1,3 +1,10 @@
+/*
+Feedback - Yoki Suprayogi
+
+untuk latihan coba tampilkan nilai -1 jika ada lebih 1 angka yang menjadi modus. 
+contoh : 7,7,8,9,9 karena modusnya 7 dan 9 maka tampilkan nilai -1
+*/
+
 function cariModus(arr) {
     // you can only write your code here!
     var newArray = [];
@@ -13,10 +20,19 @@ function cariModus(arr) {
         }
     }
 
+    /*
+    // lebih dari dua nilai modus, tampilkan nilai modus yang paling pertama muncul (dihitung dari kiri ke kanan).
     if (result.length > 0) {
         result = result[0];
     }
-    
+    */
+
+    if (result.length === 1) {
+        result = result[0];
+    } else if (result.length > 1) {
+        result = -1;
+    }
+
     if (nilaiSama === 0) {
         result = -1;
     }
@@ -33,4 +49,3 @@ console.log(cariModus([10, 4, 5, 2, 4])); // 4
 console.log(cariModus([5, 10, 10, 6, 5])); // 5
 console.log(cariModus([10, 3, 1, 2, 5])); // -1
 console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-console.log(cariModus([7, 7, 7, 7, 7])); // -1
