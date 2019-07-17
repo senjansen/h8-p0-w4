4f2153a6697a9dd1b0dbee1a0c87982a60177350
senjansen <senjansen.sj@gmail.com>
2019-07-17 17:39:27 +0700

    kondisi test case yang ngga sesuai masih belum di handle dengan baik saat mengembalikkan nilai function

--- exercise-11.js
+++ exercise-11.js
@@ -13,10 +13,10 @@ function shoppingTime(memberId, money) {
     }
 
     if (memberId === '' || money === undefined) {
-        console.log('Mohon maaf, toko X hanya berlaku untuk member saja');
+        return 'Mohon maaf, toko X hanya berlaku untuk member saja';
     }
     if (money < 50000) {
-        console.log('Mohon maaf, uang tidak cukup');
+        return 'Mohon maaf, uang tidak cukup';
     } else {
         for (var i = 0; i < barang.harga.length; i++) {
             if (money >= barang.harga[i]) {
@@ -39,12 +39,12 @@ console.log(shoppingTime('1820RzKrnWn08', 2475000));
 //    'Sweater Uniklooh',
 //    'Casing Handphone' ],
 // changeMoney: 0 }
-console.log(shoppingTime('82Ku8Ma742', 170000));
-//{ memberId: '82Ku8Ma742',
-// money: 170000,
-// listPurchased:
-//  [ 'Casing Handphone' ],
-// changeMoney: 120000 }
+console.log(shoppingTime('82Ku8Ma742', 170000)); 
+// //{ memberId: '82Ku8Ma742',
+// // money: 170000,
+// // listPurchased:
+// //  [ 'Casing Handphone' ],
+// // changeMoney: 120000 }
 console.log(shoppingTime('', 2475000)); //Mohon maaf, toko X hanya berlaku untuk member saja
 console.log(shoppingTime('234JdhweRxa53', 15000)); //Mohon maaf, uang tidak cukup
 console.log(shoppingTime()); ////Mohon maaf, toko X hanya berlaku untuk member saja
